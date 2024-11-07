870d13e037fc7be11d6530df47b72e8685929503
olivier <olivier.ernst0411@orange.fr>
2024-11-07 23:18:40 +0100

    add rainbow.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,18 @@
-require 'core.options' -- Load general options
-require 'core.keymaps' -- Load general keymaps
-require 'core.snippets' -- Custom code snippets
+require('core.options') -- Load general options
+require('core.keymaps') -- Load general keymaps
+require('core.snippets') -- Custom code snippets
 
 -- Install package manager
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
+  vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
     '--branch=stable', -- latest stable release
     lazypath,
-  }
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -20,28 +20,29 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Setup plugin
 require('lazy').setup({
-  require 'plugins.telescope',
-  require 'plugins.treesitter',
-  require 'plugins.notify',
-  require 'plugins.lsp',
-  require 'plugins.autocompletion',
-  require 'plugins.none-ls',
-  require 'plugins.lualine',
-  require 'plugins.floaterm',
-  require 'plugins.bufferline',
-  require 'plugins.neo-tree',
-  require 'plugins.alpha',
-  require 'plugins.indent-blankline',
-  require 'plugins.lazygit',
-  require 'plugins.comment',
-  require 'plugins.debug',
-  require 'plugins.gitsigns',
-  require 'plugins.database',
-  require 'plugins.misc',
-  require 'plugins.emoji',
-  require 'plugins.noice',
-  require 'plugins.themes',
-  require 'plugins.themery',
+  require('plugins.telescope'),
+  require('plugins.treesitter'),
+  require('plugins.notify'),
+  require('plugins.lsp'),
+  require('plugins.autocompletion'),
+  require('plugins.none-ls'),
+  require('plugins.lualine'),
+  require('plugins.floaterm'),
+  require('plugins.bufferline'),
+  require('plugins.neo-tree'),
+  require('plugins.alpha'),
+  require('plugins.indent-blankline'),
+  require('plugins.lazygit'),
+  require('plugins.comment'),
+  require('plugins.debug'),
+  require('plugins.gitsigns'),
+  require('plugins.database'),
+  require('plugins.rainbow'),
+  require('plugins.misc'),
+  require('plugins.emoji'),
+  require('plugins.noice'),
+  require('plugins.themes'),
+  require('plugins.themery'),
 }, {
   ui = {
     -- If you have a Nerd Font, set icons to an empty table which will use the
