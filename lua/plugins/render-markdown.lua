b9f971d44f140259de25b073bbb6b8541867229b
olivier <olivier.ernst0411@orange.fr>
2026-03-09 17:31:43 +0100

    correct luasnip.lua

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -4,6 +4,7 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    latex = { enable = nil },
+  },
 }
